fca85eb1b9ff9df41ee2eb1fbc1a85304a77d724
Takeshi Chiba <bushi.chiba@gmail.com>
2020-03-22 20:11:03 +0900

    ユーザーマイグレーション編集

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('id', 64)->unique()->comment('ラインID')->nullable();
+            $table->string('display_name')->comment('ラインの表示名')->nullable();
+            $table->string('sei')->comment('性')->nullable();
+            $table->string('mei')->comment('名')->nullable();
+            $table->string('sei_kana')->comment('セイ')->nullable();
+            $table->string('mei_kana')->comment('メイ')->nullable();
+            $table->integer('tel')->comment('電話番号')->nullable();
+            $table->string('email')->unique()->comment('メールアドレス')->nullable();
+            $table->timestamp('email_verified_at')->nullable()->nullable();
+            $table->string('password')->comment('パスワード')->nullable();
             $table->rememberToken();
+            $table->timestamp('blocked_at')->unique()->nullable();
             $table->timestamps();
         });
     }
