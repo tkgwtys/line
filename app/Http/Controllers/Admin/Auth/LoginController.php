69bec8ebf7fa6826333b77a829eb76137cbc4df5
Takeshi Chiba <bushi.chiba@gmail.com>
2020-05-13 11:56:14 +0900

    #22 管理画面ログイン。不具合対応

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -46,7 +46,9 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        session()->flush();
+        url()->previous('/admin');
+   //     $this->middleware('admins')->except('logout');
     }
 
     public function logout(Request $request)
