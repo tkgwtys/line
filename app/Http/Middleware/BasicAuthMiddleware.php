d628673ca8c35b84f9ec0facae4e8c22b5e783ff
Takeshi Chiba <bushi.chiba@gmail.com>
2020-05-19 09:00:29 +0900

    #30 ベーシック認証をかける

---
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class BasicAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        switch (true) {
+            case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+            case $_SERVER['PHP_AUTH_USER'] !== 'abihc':
+            case $_SERVER['PHP_AUTH_PW'] !== '!sMSZ&uq^3a':
+                header('WWW-Authenticate: Basic realm="Enter username and password."');
+                header('Content-Type: text/plain; charset=utf-8');
+                die('このページを見るにはログインが必要です');
+        }
+        header('Content-Type: text/html; charset=utf-8');
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,40 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => 'basicauth'], function () {
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-/**
- * ログイン前
- */
-Route::group(['prefix' => 'admin', 'middleware' => 'guest:admin'], function () {
     Route::get('/', function () {
-        return view('admin.welcome');
+        return view('welcome');
     });
-    Route::get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Admin\Auth\LoginController@login')->name('admin.login');
-    Route::get('register', 'Admin\Auth\RegisterController@showRegistrationForm')->name('admin.register');
-    Route::post('register', 'Admin\Auth\RegisterController@register')->name('admin.register');
-    Route::get('password/rest', 'Admin\Auth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-});
 
-/**
- * ログイン後
- */
-Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
-    Route::get('/', function () {
-        return view('admin.welcome');
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    /**
+     * ログイン前
+     */
+    Route::group(['prefix' => 'admin', 'middleware' => 'guest:admin'], function () {
+        Route::get('/', function () {
+            return view('admin.welcome');
+        });
+        Route::get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
+        Route::post('login', 'Admin\Auth\LoginController@login')->name('admin.login');
+        Route::get('register', 'Admin\Auth\RegisterController@showRegistrationForm')->name('admin.register');
+        Route::post('register', 'Admin\Auth\RegisterController@register')->name('admin.register');
+        Route::get('password/rest', 'Admin\Auth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
     });
-    Route::post('logout', 'Admin\Auth\LoginController@logout')->name('admin.logout');
-    Route::get('home', 'Admin\HomeController@index')->name('admin.home');
-    Route::resource('user', 'UserController');
-    Route::resource('player', 'PlayerController');
+
+    /**
+     * ログイン後
+     */
+    Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
+        Route::get('/', function () {
+            return view('admin.welcome');
+        });
+        Route::post('logout', 'Admin\Auth\LoginController@logout')->name('admin.logout');
+        Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+        Route::resource('user', 'UserController');
+        Route::resource('player', 'PlayerController');
+    });
 });
