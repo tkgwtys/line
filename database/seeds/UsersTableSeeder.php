224de6ceed73eeeb4dd3bfd3168e416f9fcedca1
Takeshi Chiba <bushi.chiba@gmail.com>
2020-05-20 15:16:36 +0900

    seed編集

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,9 +13,40 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
-                'id' => (string)Str::orderedUuid(),
+                'id' => 'U8299f8c45d021558f0d6e0355e5ccc6c',
                 'name' => 'chiba',
+                'sei' => '千葉',
+                'mei' => '武士',
+                'sei_hira' => 'ちば',
+                'mei_hira' => 'たけし',
+                'tel' => '08032015381',
                 'email' => 'bushi.chiba@abihc.com',
+                'password' => bcrypt('testtest'),
+            ],
+        ]);
+        DB::table('users')->insert([
+            [
+                'id' => 'U84b55b6739d5f7ccce8f63de3307f857',
+                'name' => 'NAR🐘',
+                'sei' => '佐渡',
+                'mei' => '永寛',
+                'sei_hira' => 'さど',
+                'mei_hira' => 'ながひろ',
+                'tel' => '09086719258',
+                'email' => 'nagaric@gmail.com',
+                'password' => bcrypt('testtest'),
+            ],
+        ]);
+        DB::table('users')->insert([
+            [
+                'id' => 'Uaedd4989ae9462602187d6d65fb150e2',
+                'name' => 'Ryota',
+                'sei' => '平野',
+                'mei' => '亮太',
+                'sei_hira' => 'ひらの',
+                'mei_hira' => 'りょうた',
+                'tel' => '09086719258',
+                'email' => 'nobori-satuma@live.jp',
                 'password' => bcrypt('testtest'),
             ],
         ]);
