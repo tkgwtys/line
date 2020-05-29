57e36ffa929ea408c50e363765fb95c469ab023c
Takeshi Chiba <bushi.chiba@gmail.com>
2020-05-29 11:19:36 +0900

    #25 予約モーダル更新

--- resources/js/common.js
+++ resources/js/common.js
@@ -12,26 +12,27 @@
 
 $('#target-table td').on('click', function () {
     // 時間ID取得
-    const tdId = $(this)[0].id;
-    console.log(tdId);
-    // Modal Inputの id='aaTime' value=''に値送信
-    document.getElementById('aaTime').value = tdId;
+    // const tdId = $(this)[0].id;
+    const day = $(this).data('day');
+    const time = $(this).data('time');
+    $('#day').val(day);
+    $('#time').val(time);
 });
 
 const flatpickr = require('flatpickr');
 const japan = require('flatpickr/dist/l10n/ja.js').default.ja;
 console.log(japan);
 flatpickr('.selector', {
-    enableTime: true, // タイムピッカーを有効
-    enableSeconds: false, // '秒' を無効
+    // enableTime: true, // タイムピッカーを有効
+    // enableSeconds: false, // '秒' を無効
     time_24hr: false, // 24時間表示
     defaultHour: 7, // 時
     defaultMinute: 0, // 分
     minTime: "07:00",
     maxTime: "23:45",
-    dateFormat: 'Y年m月d日 H:i',
+    dateFormat: 'Y年m月d日',
     locale: japan,
-    minuteIncrement: 15,
+    // minuteIncrement: 15,
 });
 
 
