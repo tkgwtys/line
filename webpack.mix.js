99d745ee2cf95020c4e8e0bd859599e7983feffa
Ryota <nobori.satuma@gmail.com>
2020-06-01 23:34:39 +0200

    #45 コンパイル調査完了

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ const mix = require('laravel-mix');
 // mix.js('resources/js/app.js', 'public/js')
 //     .js('resources/js/common.js', 'public/js')
 //     .sass('resources/sass/app.scss', 'public/css');
-mix.js(['resources/js/app.js', 'resources/js/common.js', 'node_modules/flatpickr/dist/flatpickr.js'], 'public/js')
+mix.js(['resources/js/app.js', 'resources/js/common.js','resources/js/fixed_midashi.js', 'node_modules/flatpickr/dist/flatpickr.js'], 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .stylus('node_modules/flatpickr/src/style/flatpickr.styl', 'public/css');
