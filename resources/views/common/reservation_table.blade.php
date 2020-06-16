b551eb6c6c33c5153df4cf047b25075949a769d9
Ryota <nobori.satuma@gmail.com>
2020-06-16 11:23:43 +0200

    65 reservation_table.blade.php 微修正

--- resources/views/common/reservation_table.blade.php
+++ resources/views/common/reservation_table.blade.php
@@ -44,7 +44,7 @@
 
                                                 {{$reservation->sei}}{{$reservation->mei}}
                                                 【{{$reservation->name}}（{{$reservation->course_time}}分）】
-                                                
+
                                             </div>
                                         @endif
                                     @endforeach
@@ -121,6 +121,23 @@
                             style="display: none">
                         </div>
                     </div>
+                    <!-- 店舗 -->
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label for="store">店舗</label>
+                            <select class="form-control form-control-lg" id="store" name="store">
+                                <option value="">選択してください</option>
+                                @foreach($stores as $key => $store)
+                                    <option value="{{$store->id}}">{{$store->name}}</option>
+                                @endforeach
+                            </select>
+                            <div
+                                id="err_store"
+                                class="alert alert-danger"
+                                role="alert"
+                                style="display: none"></div>
+                        </div>
+                    </div>
                     <!-- コース -->
                     <div class="form-group">
                         <label for="course">コース</label>
@@ -164,30 +181,7 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- 店舗 -->
-                        <div class="form-group">
-                            <div class="card">
-                                <div class="card-header">
-                                    店舗情報
-                                </div>
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <label for="store">店舗</label>
-                                        <select class="form-control form-control-lg" id="store" name="store">
-                                            <option value="">選択してください</option>
-                                            @foreach($stores as $key => $store)
-                                                <option value="{{$store->id}}">{{$store->name}}</option>
-                                            @endforeach
-                                        </select>
-                                        <div
-                                            id="err_store"
-                                            class="alert alert-danger"
-                                            role="alert"
-                                            style="display: none"></div>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
+
                     {{--                    <!-- トレーナ -->--}}
                     {{--                    <div class="modal-footer">--}}
                     {{--                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>--}}
