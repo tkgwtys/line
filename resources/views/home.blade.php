84a495c047f74037b324a65acb86d626a41eb57d
Takeshi Chiba <bushi.chiba@gmail.com>
2020-06-19 14:40:27 +0900

    修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <a href="/user/edit" class="btn btn-primary btn-block btn-lg">アカウント設定</a>
-            </div>
-        </div>
+        <a href="/user/edit" class="btn btn-primary btn-block btn-lg">アカウント設定</a>
     </div>
 @endsection
