3f926f087bf014c32bd4ede21aa8a0f7051b911a
Takeshi Chiba <bushi.chiba@gmail.com>
2020-06-25 20:14:57 +0900

    修正

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -5,21 +5,27 @@
         <div id="alert_message"></div>
         <table class="table table-striped" id="user_reservation_list">
             <tbody>
-            @foreach($reservations as $reservation)
-                <tr data-tr_{{$reservation->reservation_id}}>
-                    <td>
-                        {{$reservation->reservations_reserved_at}}
-                    </td>
-                    <td align="right">
-                        <button
-                            type="button"
-                            data-reservation_id="{{$reservation->reservation_id}}"
-                            class="btn btn-danger reservation_cancel_button">
-                            キャンセル
-                        </button>
-                    </td>
-                </tr>
-            @endforeach
+            @if(count($reservations) > 0)
+                @foreach($reservations as $reservation)
+                    <tr data-tr_{{$reservation->reservation_id}}>
+                        <td>
+                            {{$reservation->reservations_reserved_at}}
+                        </td>
+                        <td align="right">
+                            <button
+                                type="button"
+                                data-reservation_id="{{$reservation->reservation_id}}"
+                                class="btn btn-danger reservation_cancel_button">
+                                キャンセル
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                <div class="alert alert-warning" role="alert">
+                    現在予約はございません。
+                </div>
+            @endif
             </tbody>
         </table>
     </div>
