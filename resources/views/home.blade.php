d2d51bdde19174d9ea5347b41f6a6d5f7378697a
Takeshi Chiba <bushi.chiba@gmail.com>
2020-09-24 14:06:44 +0900

    #103 修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
     <div class="container">
         {{ Breadcrumbs::render('home') }}
-        <a href="/reservation" class="btn btn-primary btn-block btn-lg">予約確認</a>
+        <p>{{$user->sei}}{{$user->mei}}さん、こんにちは。</p>
+        <a href="/reservation" class="btn btn-primary btn-block btn-lg">予約確認・キャンセル</a>
         <a href="/user/edit" class="btn btn-primary btn-block btn-lg">アカウント設定</a>
     </div>
 @endsection
