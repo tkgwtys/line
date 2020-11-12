3c495b657aa3301148859afe3ed10383ad8121fc
Takeshi Chiba <bushi.chiba@gmail.com>
2020-11-12 09:50:52 +0900

    修正

--- resources/views/admin/player/index.blade.php
+++ resources/views/admin/player/index.blade.php
@@ -10,8 +10,7 @@
                         <a href="{{url('/admin/player/'. $player->id . '?start_date='. $today. '&day_count=7')}}">
                             {{$player->sei}}{{$player->mei}}
                         </a>
-                        <img src="{{ asset('storage/images/users/'. $player->id .'/300x300.jpg'). '?' . time() }}"
-                             width="50">
+                        <img src="{{ asset('storage/images/users/'. $player->id .'/w300.jpg'). '?' . time() }}" width="50">
                     </td>
                     <td align="right">
                         <a href="{{url('/admin/user/'.$player->id.'/edit')}}" class="btn btn-warning">編集</a>
